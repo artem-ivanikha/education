4a8136245d3b35ff543b7d4ebb6145c9624523de
Ivanikha, Artem <artem.ivanikha@itglobal.com>
2024-03-25 14:55:08 +0300

    newtasks

---
+++ lession-3/task7.py
@@ -0,0 +1,15 @@
+# Написать скрипт, который следит за местом на партиции.
+# Если свободного места осталось менее 50%, записать в лог об этом в формате
+# день-месяц-год час-минута-секунда myDiskChecker: <partition> - less than 50%
+
+# Если свободного места осталось менее 10%, записать в лог об этом в формате
+# день-месяц-год час-минута-секунда myDiskChecker: <partition> - less than 10%
+part=root
+file=./task7.log
+touch "$file"
+while true; do
+    use=$(df | awk '/'"$part"'/''{print $5}' | head -1 -c -2 );
+    ([[ $use -ge "90" ]] && echo "$(date +%e-%m-%y" "%H-%M-%S)" "myDiskChecker $part - less than 10%" || echo $use ) >> $file
+    ([[ $use -ge "50" ]] && echo "$(date +%e-%m-%y" "%H-%M-%S)" "myDiskChecker $part - less than 50%" || echo $use ) >> $file
+    sleep 1
+done
